1933b83e960647c174eb71748ad18cf15bcfff60
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-10-16 20:50:38 +0545

    Register our new model Member to admin

--- ultimatewebsite/members/admin.py
+++ ultimatewebsite/members/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from members.models import Member
 # Register your models here.
+
+class MemberAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Member
+    '''
+    list_display = ('full_name', 'email', 'phone_number',)
+
+admin.site.register(Member, MemberAdmin)
