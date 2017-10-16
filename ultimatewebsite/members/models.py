e1243cc137c487b17b6cf5a7a46088b67a506b34
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-10-16 20:49:51 +0545

    Adds a new model 'Member' which models any member of our class

--- ultimatewebsite/members/models.py
+++ ultimatewebsite/members/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    full_name = models.CharField("Full Name", max_length = 150)
+    image = models.ImageField(upload_to = 'members')
+    phone_number = models.CharField('Phone Number', max_length = 10, blank = True, unique = True)
+    email = models.CharField('Email', max_length = 150)
+    hometown = models.CharField("Your HomeTown", max_length = 200)
+    favourite_quote = models.CharField(max_length = 100, blank = True)
+    bio = models.TextField()
+    your_website = models.URLField()
+    facebook_url = models.CharField('Facebook URL', max_length = 100,
+                     blank = True)
+    twitter_url = models.CharField('Twitter URL', max_length = 100,
+                     blank = True)
+    instagram_url = models.CharField('Instagram URL', max_length = 100,
+     blank = True)
+    github_url = models.CharField('Github URL', max_length = 100,
+                     blank = True)
+    class Meta:
+        verbose_name = "Member"
+        verbose_name_plural = "Members"
+
+    def __str__(self):
+        return self.full_name
