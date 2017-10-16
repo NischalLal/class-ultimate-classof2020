288446e17bf4a24fdcc8ebf90b65f5df2bf473f5
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-10-16 20:52:58 +0545

    Adds method to add new member, update member and other

--- ultimatewebsite/members/views.py
+++ ultimatewebsite/members/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, HttpResponse,\
+                            HttpResponseRedirect
+from members.forms import MemberForm
+from members.models import Member
+from django.urls import reverse
 # Create your views here.
+
+def member_list(request):
+    all_member = Member.objects.all().order_by('full_name')
+    template_name = 'members/list.html'
+    context = {
+        'all_member': all_member
+    }
+    return render(request, template_name, context)
+
+def member_detail(request, pk):
+    member = get_object_or_404(Member, pk=pk)
+    return render(request, 'members/detail.html', {'member':member})
+
+def add_member(request):
+    if request.method == 'POST':
+        form = MemberForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('Member List', args = []))
+    else:
+        form = MemberForm()
+    return render(request, 'members/new_member.html', {'form':form})
+
+def update_member_info(request, pk):
+    member = get_object_or_404(Member, pk=pk)
+    if request.method == 'POST':
+        form = MemberForm(request.POST, request.FILES, instance = member)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('Member Detail', 
+                kwargs = {'pk':pk}))
+    else:
+        form = MemberForm(instance = member)
+    template_name = 'members/new_member.html'
+    context = {
+    'form':form
+    }
+    return render(request, template_name, context)
