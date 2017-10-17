17eb6fffb810dd7fd91a4886743d459758e0c9cc
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-10-17 10:58:32 +0545

    Reinitialied migrations

--- ultimatewebsite/members/migrations/0002_auto_20171016_1645.py
+++ ultimatewebsite/members/migrations/0002_auto_20171016_1645.py
@@ -7,6 +7,10 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
+    dependencies = [
+             ('members', '0001_initial'),
+         ]
+         
     operations = [
         migrations.AlterField(
             model_name='member',
