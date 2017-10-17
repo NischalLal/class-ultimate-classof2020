9e419541b9e84206e9b8a51ce58950e956faa1c6
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-10-17 10:41:25 +0545

    Update 0002_auto_20171016_1645.py
    Completely Removing Dependenies

--- ultimatewebsite/members/migrations/0002_auto_20171016_1645.py
+++ ultimatewebsite/members/migrations/0002_auto_20171016_1645.py
@@ -6,11 +6,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-"""
-    dependencies = [
-        ('members', '0001_initial'),
-    ]
-  """
 
     operations = [
         migrations.AlterField(
