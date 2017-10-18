80304747683ae24b26e6a069bd14b3f5ef9f94e3
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-10-18 23:22:35 +0545

    Adds Button to download list along with some icons

--- ultimatewebsite/downloads/urls.py
+++ ultimatewebsite/downloads/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from downloads.views import download_item, download_list
+from downloads.views import download_item, download_list, add_download_item
 
 urlpatterns = [
         url(r'^(?P<id>\d+)/$', download_item, name = 'Download Item'),
         url(r'^all-downloads/', download_list, name = 'Download List'),
-
+        url(r'^upload-file/', add_download_item, name = 'Upload File'),
 ] 
--- ultimatewebsite/downloads/views.py
+++ ultimatewebsite/downloads/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render, get_object_or_404, HttpResponse,\
                             HttpResponseRedirect
 from django.urls import reverse
 from downloads.models import DownloadItem
+from downloads.forms import DownloadItemForm
 import os
 from django.http import FileResponse
 from django.utils.text import slugify
 # Create your views here.
+
+def add_download_item(request):
+    if request.method == 'POST':
+        form = DownloadItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('Download List'))
+    else:
+        form = DownloadItemForm()
+    return render(request, 'downloads/new_download_item.html', {'form':form})
 
 def download_item(request, id):
     item = get_object_or_404(DownloadItem, pk=id)
