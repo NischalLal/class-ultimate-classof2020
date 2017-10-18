ebfb1c943e59eca4c5a0b9d54635ff2da242e338
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-10-18 13:37:51 +0545

    Changes blank url to show member list, now default url will show all the member list

--- ultimatewebsite/ultimatewebsite/urls.py
+++ ultimatewebsite/ultimatewebsite/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from members.views import member_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', member_list, name = 'Member List Home'),
     url(r'^members/', include('members.urls')),
     url(r'^blog/', include('blogs.urls')),
 ]
