4d0642e3fefaf6dd91d18e0f49c5e9c9cedb4786
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-10-18 09:23:01 +0545

    Adds New form to add comment and post

---
+++ ultimatewebsite/blogs/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from blogs.models import BlogPost, Comment
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'main_image', 'text')
+
+
+class NewCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment_text',)
